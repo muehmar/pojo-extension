ebee9da993191a8f070a16278ce45fff47604a6c
Martin Mühlemann <poelsi@gmail.com>
2021-09-28 13:19:24 +0200

    Add JavaModifiers and Type to ClassGenerator

--- generator/src/main/java/io/github/muehmar/pojoextension/generator/impl/gen/ClassGenerator.java
+++ generator/src/main/java/io/github/muehmar/pojoextension/generator/impl/gen/ClassGenerator.java
@@ -7,17 +7,19 @@ import io.github.muehmar.pojoextension.generator.Generator;
 import io.github.muehmar.pojoextension.generator.Writer;
 import io.github.muehmar.pojoextension.generator.data.Pojo;
 import io.github.muehmar.pojoextension.generator.data.PojoSettings;
+import io.github.muehmar.pojoextension.generator.impl.JavaModifier;
+import java.util.Comparator;
 
 public class ClassGenerator implements Generator<Pojo, PojoSettings> {
+  private final ClassType type;
+  private final PList<JavaModifier> modifiers;
   private final PList<Generator<Pojo, PojoSettings>> content;
 
-  public ClassGenerator(PList<Generator<Pojo, PojoSettings>> content) {
+  private ClassGenerator(
+      ClassType type, PList<JavaModifier> modifiers, PList<Generator<Pojo, PojoSettings>> content) {
+    this.type = type;
+    this.modifiers = modifiers;
     this.content = content;
-  }
-
-  @SafeVarargs
-  public static ClassGenerator ofContent(Generator<Pojo, PojoSettings>... generators) {
-    return new ClassGenerator(PList.fromArray(generators));
   }
 
   @Override
@@ -28,19 +30,72 @@ public class ClassGenerator implements Generator<Pojo, PojoSettings> {
 
     return new PackageGenerator()
         .append(newLine())
-        .append(Writer::printRefs)
-        .append(newLine())
+        .append(this::refs)
         .append(this::classStart)
         .append(1, contentGenerator)
         .append(this::classEnd)
         .generate(pojo, settings, writer);
   }
 
+  private Writer refs(Pojo pojo, PojoSettings settings, Writer writer) {
+    if (type.equals(ClassType.NESTED)) {
+      return writer;
+    }
+
+    return writer.printRefs().println();
+  }
+
   private Writer classStart(Pojo pojo, PojoSettings settings, Writer writer) {
-    return writer.println("public class %s {", pojo.getExtensionName().asString());
+    return writer.println(
+        "%sclass %s {",
+        modifiers
+            .sort(Comparator.comparingInt(JavaModifier::getOrder))
+            .map(m -> String.format("%s ", m.asString()))
+            .mkString(""),
+        pojo.getExtensionName().asString());
   }
 
   private Writer classEnd(Pojo pojo, PojoSettings settings, Writer writer) {
     return writer.println("}", pojo.getExtensionName().asString());
+  }
+
+  public static ClassGeneratorCreator1 topLevel() {
+    return new ClassGeneratorCreator1(ClassType.TOP_LEVEL);
+  }
+
+  public static ClassGeneratorCreator1 nested() {
+    return new ClassGeneratorCreator1(ClassType.NESTED);
+  }
+
+  public static final class ClassGeneratorCreator1 {
+    private final ClassType type;
+
+    private ClassGeneratorCreator1(ClassType type) {
+      this.type = type;
+    }
+
+    public ClassGeneratorCreator2 modifiers(JavaModifier... modifiers) {
+      return new ClassGeneratorCreator2(type, PList.fromArray(modifiers));
+    }
+  }
+
+  public static final class ClassGeneratorCreator2 {
+    private final ClassType type;
+    private final PList<JavaModifier> modifiers;
+
+    private ClassGeneratorCreator2(ClassType type, PList<JavaModifier> modifiers) {
+      this.type = type;
+      this.modifiers = modifiers;
+    }
+
+    @SafeVarargs
+    public final ClassGenerator content(Generator<Pojo, PojoSettings>... generators) {
+      return new ClassGenerator(type, modifiers, PList.fromArray(generators));
+    }
+  }
+
+  public enum ClassType {
+    TOP_LEVEL,
+    NESTED
   }
 }
--- generator/src/test/java/io/github/muehmar/pojoextension/generator/impl/gen/ClassGeneratorTest.java
+++ generator/src/test/java/io/github/muehmar/pojoextension/generator/impl/gen/ClassGeneratorTest.java
@@ -2,19 +2,27 @@ package io.github.muehmar.pojoextension.generator.impl.gen;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import ch.bluecare.commons.data.PList;
 import io.github.muehmar.pojoextension.generator.Generator;
 import io.github.muehmar.pojoextension.generator.Pojos;
 import io.github.muehmar.pojoextension.generator.Writer;
 import io.github.muehmar.pojoextension.generator.data.PojoSettings;
+import io.github.muehmar.pojoextension.generator.impl.JavaModifier;
 import io.github.muehmar.pojoextension.generator.impl.WriterImpl;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class ClassGeneratorTest {
 
   @Test
   void generate_when_simplePojoAndSingleContent_then_correctGeneratedString() {
     final ClassGenerator generator =
-        ClassGenerator.ofContent(Generator.ofWriterFunction(w -> w.println("Content")));
+        ClassGenerator.topLevel()
+            .modifiers(JavaModifier.PUBLIC)
+            .content(Generator.ofWriterFunction(w -> w.println("Content")));
 
     final Writer writer =
         generator.generate(
@@ -32,8 +40,9 @@ class ClassGeneratorTest {
   @Test
   void generate_when_refAddedInContent_then_refPrinted() {
     final ClassGenerator generator =
-        ClassGenerator.ofContent(
-            Generator.ofWriterFunction(w -> w.ref("import java.util.Optional;")));
+        ClassGenerator.topLevel()
+            .modifiers(JavaModifier.PUBLIC)
+            .content(Generator.ofWriterFunction(w -> w.ref("import java.util.Optional;")));
 
     final Writer writer =
         generator.generate(
@@ -46,5 +55,43 @@ class ClassGeneratorTest {
             + "public class CustomerExtension {\n"
             + "}\n",
         writer.asString());
+  }
+
+  @Test
+  void generate_when_nestedClass_then_noRefsPrinted() {
+    final ClassGenerator generator =
+        ClassGenerator.nested()
+            .modifiers(JavaModifier.PUBLIC)
+            .content(Generator.ofWriterFunction(w -> w.ref("import java.util.Optional;")));
+
+    final Writer writer =
+        generator.generate(
+            Pojos.sample(), PojoSettings.defaultSettings(), WriterImpl.createDefault());
+    assertEquals(
+        "package io.github.muehmar;\n" + "\n" + "public class CustomerExtension {\n" + "}\n",
+        writer.asString());
+  }
+
+  @ParameterizedTest
+  @MethodSource("publicAndFinalModifierUnordered")
+  void generate_when_privateAndFinalModifierUnordered_then_correctOutputWithOrderedModifiers(
+      PList<JavaModifier> modifiers) {
+    final ClassGenerator generator =
+        ClassGenerator.nested()
+            .modifiers(modifiers.toArray(JavaModifier.class))
+            .content(Generator.ofWriterFunction(w -> w.ref("import java.util.Optional;")));
+
+    final Writer writer =
+        generator.generate(
+            Pojos.sample(), PojoSettings.defaultSettings(), WriterImpl.createDefault());
+    assertEquals(
+        "package io.github.muehmar;\n" + "\n" + "public final class CustomerExtension {\n" + "}\n",
+        writer.asString());
+  }
+
+  private static Stream<Arguments> publicAndFinalModifierUnordered() {
+    return Stream.of(
+        Arguments.of(PList.of(JavaModifier.FINAL, JavaModifier.PUBLIC)),
+        Arguments.of(PList.of(JavaModifier.PUBLIC, JavaModifier.FINAL)));
   }
 }
