2ab29a36c486391c12366d1db4bed495fb4375b6
Martin Mühlemann <poelsi@gmail.com>
2021-09-28 14:27:30 +0200

    Add MethodGenerator

---
+++ generator/src/main/java/io/github/muehmar/pojoextension/generator/impl/gen/MethodGenerator.java
@@ -0,0 +1,44 @@
+package io.github.muehmar.pojoextension.generator.impl.gen;
+
+import ch.bluecare.commons.data.PList;
+import io.github.muehmar.pojoextension.generator.Generator;
+import io.github.muehmar.pojoextension.generator.Writer;
+import io.github.muehmar.pojoextension.generator.impl.JavaModifiers;
+import java.util.function.BiFunction;
+
+public class MethodGenerator<A, B> implements Generator<A, B> {
+  private final JavaModifiers modifiers;
+  private final BiFunction<A, B, String> createReturnType;
+  private final BiFunction<A, B, String> createMethodName;
+  private final BiFunction<A, B, PList<String>> createArguments;
+  private final Generator<A, B> contentGenerator;
+
+  public MethodGenerator(
+      JavaModifiers modifiers,
+      BiFunction<A, B, String> createReturnType,
+      BiFunction<A, B, String> createMethodName,
+      BiFunction<A, B, PList<String>> createArguments,
+      Generator<A, B> contentGenerator) {
+    this.modifiers = modifiers;
+    this.createReturnType = createReturnType;
+    this.createMethodName = createMethodName;
+    this.createArguments = createArguments;
+    this.contentGenerator = contentGenerator;
+  }
+
+  @Override
+  public Writer generate(A data, B settings, Writer writer) {
+    return Generator.<A, B>ofWriterFunction(
+            w -> {
+              final String returnType = createReturnType.apply(data, settings);
+              final String methodName = createMethodName.apply(data, settings);
+              final String arguments = createArguments.apply(data, settings).mkString(", ");
+              return w.print(
+                  "%s%s %s(%s) {",
+                  modifiers.asStringTrailingWhitespace(), returnType, methodName, arguments);
+            })
+        .append(1, contentGenerator)
+        .append(w -> w.println("}"))
+        .generate(data, settings, writer);
+  }
+}
---
+++ generator/src/test/java/io/github/muehmar/pojoextension/generator/impl/gen/MethodGeneratorTest.java
@@ -0,0 +1,31 @@
+package io.github.muehmar.pojoextension.generator.impl.gen;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import ch.bluecare.commons.data.PList;
+import io.github.muehmar.pojoextension.generator.impl.JavaModifier;
+import io.github.muehmar.pojoextension.generator.impl.JavaModifiers;
+import io.github.muehmar.pojoextension.generator.impl.WriterImpl;
+import org.junit.jupiter.api.Test;
+
+class MethodGeneratorTest {
+  @Test
+  void generate_when_minimalGeneratorCreated_then_outputCorrect() {
+    final MethodGenerator<PList<String>, Void> generator =
+        new MethodGenerator<>(
+            JavaModifiers.of(JavaModifier.PUBLIC, JavaModifier.FINAL),
+            (l, ignore) -> l.apply(0),
+            (l, ignore) -> l.apply(1),
+            (l, ignore) -> l.drop(2),
+            (l, ignore, w) -> w.println("System.out.println(\"Hello World\");"));
+
+    final PList<String> data = PList.of("void", "getXY", "String a", "int b");
+
+    final String output = generator.generate(data, null, WriterImpl.createDefault()).asString();
+    assertEquals(
+        "public final void getXY(String a, int b) {\n"
+            + "  System.out.println(\"Hello World\");\n"
+            + "}\n",
+        output);
+  }
+}
