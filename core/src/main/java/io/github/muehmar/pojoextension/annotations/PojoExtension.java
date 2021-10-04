72c4cfdd485294ac4e2b13339dc091eaa5a80940
Martin Mühlemann <poelsi@gmail.com>
2021-10-04 14:45:02 +0200

    Update javadoc

--- core/src/main/java/io/github/muehmar/pojoextension/annotations/PojoExtension.java
+++ core/src/main/java/io/github/muehmar/pojoextension/annotations/PojoExtension.java
@@ -5,10 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/** Creates an extension class for the annotated class. */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.SOURCE)
 public @interface PojoExtension {
 
+  /** Defines how optional fields in a pojo are detected. */
   OptionalDetection[] optionalDetection() default {
     OptionalDetection.OPTIONAL_CLASS, OptionalDetection.NULLABLE_ANNOTATION
   };
