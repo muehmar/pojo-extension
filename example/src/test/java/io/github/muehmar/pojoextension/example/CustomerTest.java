fff83c819f38460c63380593e28d168ff6451272
Martin Mühlemann <poelsi@gmail.com>
2021-10-04 15:18:47 +0200

    Add simple example project

---
+++ example/src/test/java/io/github/muehmar/pojoextension/example/CustomerTest.java
@@ -0,0 +1,25 @@
+package io.github.muehmar.pojoextension.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+
+class CustomerTest {
+  @Test
+  void newBuilder_when_usedToCreateInstance_then_allAttributesSetAccordingly() {
+    final Customer customer =
+        Customer.newBuilder()
+            .setId("123456")
+            .setName("Dexter")
+            .andAllOptionals()
+            .setNickname("Dex")
+            .setAge(Optional.empty())
+            .build();
+
+    assertEquals("123456", customer.getId());
+    assertEquals("Dexter", customer.getName());
+    assertEquals(Optional.of("Dex"), customer.getNickname());
+    assertEquals(Optional.empty(), customer.getAge());
+  }
+}
