d2b19bd4c4a6856f4ae240b7da5fbd24e2b8f203
Martin Mühlemann <poelsi@gmail.com>
2021-12-05 20:45:14 +0100

    Break possible stackoverflow caused by circular annotation path

--- example/src/main/java/io/github/muehmar/pojoextension/example/customannotations/AllRequiredExtension.java
+++ example/src/main/java/io/github/muehmar/pojoextension/example/customannotations/AllRequiredExtension.java
@@ -2,7 +2,13 @@ package io.github.muehmar.pojoextension.example.customannotations;
 
 import io.github.muehmar.pojoextension.annotations.OptionalDetection;
 import io.github.muehmar.pojoextension.annotations.PojoExtension;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.CLASS)
 @PojoExtension(optionalDetection = OptionalDetection.NONE)
 public @interface AllRequiredExtension {
   String extensionName() default "";
