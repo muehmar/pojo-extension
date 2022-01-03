cb1cdc20bdf89e3e810ce8ab3ace9a4f6d92ba8b
Martin Mühlemann <poelsi@gmail.com>
2022-01-03 20:01:06 +0100

    Add more tests

--- example/src/test/java/io/github/muehmar/pojoextension/example/customannotations/DisabledMappersClassTest.java
+++ example/src/test/java/io/github/muehmar/pojoextension/example/customannotations/DisabledMappersClassTest.java
@@ -1,11 +1,14 @@
 package io.github.muehmar.pojoextension.example.customannotations;
 
+import static io.github.muehmar.pojoextension.example.MethodHelper.EQUALS;
 import static io.github.muehmar.pojoextension.example.MethodHelper.GEN_EQUALS;
 import static io.github.muehmar.pojoextension.example.MethodHelper.GEN_HASH_CODE;
 import static io.github.muehmar.pojoextension.example.MethodHelper.GEN_TO_STRING;
+import static io.github.muehmar.pojoextension.example.MethodHelper.HASH_CODE;
 import static io.github.muehmar.pojoextension.example.MethodHelper.MAP;
 import static io.github.muehmar.pojoextension.example.MethodHelper.MAP_IF;
 import static io.github.muehmar.pojoextension.example.MethodHelper.MAP_IF_PRESENT;
+import static io.github.muehmar.pojoextension.example.MethodHelper.TO_STRING;
 import static io.github.muehmar.pojoextension.example.MethodHelper.hasMethod;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -27,5 +30,15 @@ class DisabledMappersClassTest {
     assertFalse(hasMethod(clazz, MAP));
     assertFalse(hasMethod(clazz, MAP_IF));
     assertFalse(hasMethod(clazz, MAP_IF_PRESENT));
+  }
+
+  @Test
+  void baseClassHasAllMethods() {
+    final Class<?> clazz = DisabledMappersClassBase.class;
+
+    assertTrue(hasMethod(clazz, EQUALS));
+    assertTrue(hasMethod(clazz, HASH_CODE));
+
+    assertTrue(hasMethod(clazz, TO_STRING));
   }
 }
--- example/src/test/java/io/github/muehmar/pojoextension/example/customannotations/DisabledToStringClassTest.java
+++ example/src/test/java/io/github/muehmar/pojoextension/example/customannotations/DisabledToStringClassTest.java
@@ -1,11 +1,14 @@
 package io.github.muehmar.pojoextension.example.customannotations;
 
+import static io.github.muehmar.pojoextension.example.MethodHelper.EQUALS;
 import static io.github.muehmar.pojoextension.example.MethodHelper.GEN_EQUALS;
 import static io.github.muehmar.pojoextension.example.MethodHelper.GEN_HASH_CODE;
 import static io.github.muehmar.pojoextension.example.MethodHelper.GEN_TO_STRING;
+import static io.github.muehmar.pojoextension.example.MethodHelper.HASH_CODE;
 import static io.github.muehmar.pojoextension.example.MethodHelper.MAP;
 import static io.github.muehmar.pojoextension.example.MethodHelper.MAP_IF;
 import static io.github.muehmar.pojoextension.example.MethodHelper.MAP_IF_PRESENT;
+import static io.github.muehmar.pojoextension.example.MethodHelper.TO_STRING;
 import static io.github.muehmar.pojoextension.example.MethodHelper.hasMethod;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -27,5 +30,15 @@ class DisabledToStringClassTest {
     assertTrue(hasMethod(clazz, MAP));
     assertTrue(hasMethod(clazz, MAP_IF));
     assertTrue(hasMethod(clazz, MAP_IF_PRESENT));
+  }
+
+  @Test
+  void baseClassHasNoToStringMethod() {
+    final Class<?> clazz = DisabledToStringClassBase.class;
+
+    assertTrue(hasMethod(clazz, EQUALS));
+    assertTrue(hasMethod(clazz, HASH_CODE));
+
+    assertFalse(hasMethod(clazz, TO_STRING));
   }
 }
