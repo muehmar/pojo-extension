3a76aa9ce8cb9124e758e04844dfcec07f26de58
Martin Mühlemann <poelsi@gmail.com>
2022-01-29 13:17:48 +0100

    Add generic example

--- example/src/main/java/io/github/muehmar/pojoextension/example/fieldbuilder/FieldBuilderClass.java
+++ example/src/main/java/io/github/muehmar/pojoextension/example/fieldbuilder/FieldBuilderClass.java
@@ -4,17 +4,20 @@ import io.github.muehmar.pojoextension.annotations.FieldBuilder;
 import io.github.muehmar.pojoextension.annotations.SafeBuilder;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.function.Supplier;
 
 @SafeBuilder
-public class FieldBuilderClass {
+public class FieldBuilderClass<T> {
   private final String prop1;
   private final String prop2;
   private final Optional<String> prop3;
+  private final Optional<T> data;
 
-  public FieldBuilderClass(String prop1, String prop2, Optional<String> prop3) {
+  FieldBuilderClass(String prop1, String prop2, Optional<String> prop3, Optional<T> data) {
     this.prop1 = prop1;
     this.prop2 = prop2;
     this.prop3 = prop3;
+    this.data = data;
   }
 
   public String getProp1() {
@@ -27,6 +30,10 @@ public class FieldBuilderClass {
 
   public Optional<String> getProp3() {
     return prop3;
+  }
+
+  public Optional<T> getData() {
+    return data;
   }
 
   @FieldBuilder(fieldName = "prop1")
@@ -49,5 +56,10 @@ public class FieldBuilderClass {
     static String constant() {
       return "CONSTANT";
     }
+  }
+
+  @FieldBuilder(fieldName = "data")
+  static <T> T supplier(Supplier<T> s) {
+    return s.get();
   }
 }
--- example/src/test/java/io/github/muehmar/pojoextension/example/fieldbuilder/FieldBuilderClassTest.java
+++ example/src/test/java/io/github/muehmar/pojoextension/example/fieldbuilder/FieldBuilderClassTest.java
@@ -9,32 +9,47 @@ import org.junit.jupiter.api.Test;
 class FieldBuilderClassTest {
   @Test
   void randomString_when_twoInstancesCreated_then_notEqualsProp1() {
-    final FieldBuilderClass fieldBuilder1 =
-        FieldBuilderClassBuilder.create().randomString().prop2("asd").build();
-    final FieldBuilderClass fieldBuilder2 =
-        FieldBuilderClassBuilder.create().randomString().prop2("asd").build();
+    final FieldBuilderClass<String> fieldBuilder1 =
+        FieldBuilderClassBuilder.<String>create().randomString().prop2("asd").build();
+    final FieldBuilderClass<String> fieldBuilder2 =
+        FieldBuilderClassBuilder.<String>create().randomString().prop2("asd").build();
 
     assertNotEquals(fieldBuilder1.getProp1(), fieldBuilder2.getProp1());
   }
 
   @Test
   void fromInt_when_usedInBuilder_then_prop1IsIntegerString() {
-    final FieldBuilderClass fieldBuilder =
-        FieldBuilderClassBuilder.create().fromInt(52).prop2("asd").build();
+    final FieldBuilderClass<String> fieldBuilder =
+        FieldBuilderClassBuilder.<String>create().fromInt(52).prop2("asd").build();
 
     assertEquals("52", fieldBuilder.getProp1());
   }
 
   @Test
   void constant_when_usedInBuilder_then_prop3WithConstantValue() {
-    final FieldBuilderClass fieldBuilder =
-        FieldBuilderClassBuilder.create()
+    final FieldBuilderClass<String> fieldBuilder =
+        FieldBuilderClassBuilder.<String>create()
             .randomString()
             .prop2("asd")
             .andAllOptionals()
             .constant()
+            .data("")
             .build();
 
     assertEquals(Optional.of("CONSTANT"), fieldBuilder.getProp3());
+  }
+
+  @Test
+  void supplier_when_usedInBuilder_then_dataFilledFromSupplier() {
+    final FieldBuilderClass<String> fieldBuilder =
+        FieldBuilderClassBuilder.<String>create()
+            .randomString()
+            .prop2("asd")
+            .andAllOptionals()
+            .constant()
+            .supplier(() -> "supplierData")
+            .build();
+
+    assertEquals(Optional.of("supplierData"), fieldBuilder.getData());
   }
 }
